23f41efe3c7ef28416b310274de569bd2cdb9983
Gabriel <gabriel.calvente@epita.fr>
2022-02-23 20:28:41 +0100

    Switched movement mode to legacy instead of new input system

---
+++ Unity/Test/Assets/PlayerInteractions/PlayerMovements.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public void Jump()
+    {
+        Debug.Log("jump");
+    }
+
+}
---
+++ Unity/UNIT404/Assets/Gabriel Gameplay/Movements/PlayerInput/PlayerMoves.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour
+{
+    public bool jumpPressed;
+    public float speed = 0.75f;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody rigidbodyComponent;
+    [SerializeField] private Transform groundCheckTansform = null;
+
+    private void Start()
+    {
+        rigidbodyComponent = GetComponent < Rigidbody > ();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpPressed = true;
+        }
+       
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, verticalInput);
+
+        if (Physics.OverlapSphere(groundCheckTansform.position, 0.1f).Length == 1)
+        {
+            return;
+        }
+        if (jumpPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 10, ForceMode.VelocityChange);
+            jumpPressed = false;
+        }      
+    }
+
+}
