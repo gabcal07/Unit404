5d8feb84930835b2ab1cba7ea3afa61c65dd71df
paulnrs <paul.rousseau@epita.fr>
2022-02-24 13:16:46 +0100

    Debut UI in game
    menu in game pour pause

---
+++ Unity/UNIT404/Assets/Paul/Pause.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool IsGamePaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // "scene du menu" entre guillemets
+        Time.timeScale = 1f;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quit");
+    }
+    
+}
