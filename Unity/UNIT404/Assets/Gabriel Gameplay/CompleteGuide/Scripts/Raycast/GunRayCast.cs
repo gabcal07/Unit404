d616adbfbcfc3fce29327682114edbeecbf2e23d
Gabriel <gabriel.calvente@epita.fr>
2022-04-20 15:13:14 +0200

    Ammunation and Reloading system added

--- Unity/UNIT404/Assets/Gabriel Gameplay/CompleteGuide/Scripts/Raycast/GunRayCast.cs
+++ Unity/UNIT404/Assets/Gabriel Gameplay/CompleteGuide/Scripts/Raycast/GunRayCast.cs
@@ -8,14 +8,37 @@ public class GunRayCast : MonoBehaviour
     public float range = 100f;
     public float firerate;
     public float impactForce;
+
+    public int maxAmmo = 30;
+    private int currentAmmo;
+    public float reloadTime = 1;
+    private bool isReloading = false;
+
     public GameObject firingPoint;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
-    private float nextTimeToFire = 0f; 
+    private float nextTimeToFire = 0f;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+    }
     void Update()
     {
+        if (isReloading)
+            return;
 
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / firerate;
@@ -23,8 +46,17 @@ public class GunRayCast : MonoBehaviour
         }
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
+        currentAmmo--;
         muzzleFlash.Play(); 
         RaycastHit hit;
 
